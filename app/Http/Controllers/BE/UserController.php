540c28cb57fc7f4276dbd5c69903a2870ecda07a
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-08-10 23:38:53 +0700

    Create Register User Page

--- app/Http/Controllers/BE/UserController.php
+++ app/Http/Controllers/BE/UserController.php
@@ -3,14 +3,16 @@
 namespace App\Http\Controllers\BE;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\UserStoreRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Http\Response
      */
     public function index() {
         $users = User::all();
@@ -30,10 +32,18 @@ class UserController extends Controller {
      * Store a newly created resource in storage.
      *
      * @param \Illuminate\Http\Request $request
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\RedirectResponse
      */
-    public function store(Request $request) {
-        //
+    public function store(UserStoreRequest $request) {
+        User::create([
+            'username' => $request->username,
+            'first_name' => $request->first_name,
+            'last_name' => $request->last_name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return redirect()->route('users.index')->with('message', "User Register Successfully!");
     }
 
     /**
@@ -50,7 +60,7 @@ class UserController extends Controller {
      * Show the form for editing the specified resource.
      *
      * @param int $id
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Http\Response
      */
     public function edit(User $user) {
         return view('users.edit', compact('user'));
